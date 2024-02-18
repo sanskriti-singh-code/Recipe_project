8d104188ecc023ebbd55d7b5442e37125d03665f
sanskriti singh <86918638+sanskriti-singh-code@users.noreply.github.com>
2024-02-18 20:09:55 +0530

    Add files via upload

---
+++ vegge/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Recipe(models.Model):
+    user=models.ForeignKey(User,on_delete=models.SET_NULL , null=True , blank=True)
+    recipe_name=models.CharField(max_length=100)
+    recipe_description= models.TextField()
+    recipe_image=models.ImageField(upload_to="recipe")
+    recipe_view_count = models.IntegerField(default=1)
+
+class Department(models.Model):
+    department=models.CharField(max_length=100)
+    def __str__(self) -> str:
+        return self.department
+    
+    class Meta:
+        ordering=["department"]
+
+class StudentID(models.Model):
+    student_id=models.CharField(max_length=100)
+
+    def __str__(self)-> str:
+        return self.student_id
+    
+class Student(models.Model):
+    department=models.ForeignKey(Department, related_name="depart",on_delete=models.CASCADE)
+    student_id=models.OneToOneField(StudentID, related_name="studentid", on_delete=models.CASCADE)
+    student_name=models.CharField(max_length=100)
+    student_email=models.EmailField(unique=True)
+    student_age=models.IntegerField(default=18)
+    student_address=models.TextField()
+
+    def __str__(self)-> str:
+        return self.student_name
+    
+    class Meta:
+        ordering = ['student_name']
+        verbose_name ='Student'
